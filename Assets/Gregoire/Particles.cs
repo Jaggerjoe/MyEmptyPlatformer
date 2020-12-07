2b84cc05af01107eb4208037247eb62751405b58
JaggerJoe <gregoire.thomassin@e-artsup.net>
2020-12-07 18:30:05 +0100

    VFX

---
+++ Assets/Gregoire/Particles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_ParticlesCoin = null;
+    private SoundManager m_SoundManger = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_SoundManger = FindObjectOfType<SoundManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Instantiate(m_ParticlesCoin, transform.position, transform.rotation);
+        m_SoundManger.Play("Coin");
+        Debug.Log("J'ai touché");
+    }
+}
---
+++ Assets/Gregoire/SoundManage/SoundScriptable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewSound", menuName = "Sound")]
+public class SoundScriptable : ScriptableObject
+{
+    public string naming;
+
+    public AudioClip clip;
+
+    [Range(0, 1)]
+    public float volume;
+
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
