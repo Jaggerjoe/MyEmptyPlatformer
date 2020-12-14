9aaf430906c47d0ad1e54677d7a0bfbbf31055de
JaggerJoe <gregoire.thomassin@e-artsup.net>
2020-12-14 18:32:14 +0100

    anim

---
+++ Assets/Gregoire/Script/Instantiator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class Instantiator : ScriptableObject
+{
+    [SerializeField]
+    private GameObject m_Prefab = null;
+
+    public void InstantiatePref(CollectInfos p_Infos)
+    {
+        Instantiate(m_Prefab, p_Infos.position, Quaternion.identity);
+    }
+}
