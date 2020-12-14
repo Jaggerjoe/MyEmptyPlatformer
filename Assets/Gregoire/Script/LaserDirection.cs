a405c6a56db70a26495c9da17dd5d710dcb8c4c6
JaggerJoe <gregoire.thomassin@e-artsup.net>
2020-12-14 20:23:41 +0100

    tiles

---
+++ Assets/Gregoire/Script/LaserDirection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserDirection : MonoBehaviour
+{
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= .2 && gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+            timer = 0;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.DrawRay(transform.position, m_Shoot.AimVector);
+    }
+}
